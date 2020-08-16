a750848c512f794e180897e99e369cfd79f08ec7
Christopher Speakes <13alvone@gmail.com>
2020-08-15 21:06:38 -0400

    Added boilerplate elapsed time reporter function for all projects.

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 import argparse
+import math
+import time
 
 
 def parse_args():
@@ -11,9 +13,24 @@ def parse_args():
     return arguments
 
 
+def print_elapsed_time(_start_time):
+    seconds = round(int(time.time() - _start_time), 2)
+    minutes = math.trunc(seconds / 60)
+    remaining_seconds = math.trunc(seconds - (minutes * 60))
+    if len(f'{remaining_seconds}') != 2:
+        remaining_seconds = f'0{remaining_seconds}'
+    elapsed_time = f'{minutes}:{remaining_seconds}'
+    msg = '**** Total_Time Elapsed: ' + elapsed_time + ' =======================\n\n'
+    print(msg)
+
+
 def main():
     args = parse_args()
     ip = args.ip
     port = args.port
     username = args.username
-    # Do Stuff Here and Modify Above argparse with options as ncessary.
+    # Do Stuff Here and Modify Above argparse with options as necessary.
+
+
+if __name__ == '__main__':
+    main()
